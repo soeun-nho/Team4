0cf666d705ca3abc06a9beda0bf52afdd22707fd
hyobin-yang <hyobin3026@naver.com>
2023-11-11 20:49:18 +0900

    [Feat]: price 수정

--- shareFood/home/models.py
+++ shareFood/home/models.py
@@ -12,7 +12,7 @@ class Delivery(models.Model):
     title = models.CharField(max_length=100, verbose_name="글제목")
     created_at = models.DateTimeField(verbose_name="구매 날짜와 시각", auto_now_add=True)
     location = models.CharField(max_length=100, verbose_name="위치")
-    minimumPrice = models.IntegerField(verbose_name="최소주문금액", null=False) 
+    price = models.IntegerField(verbose_name="최소주문금액", null=False) 
     link = models.CharField(max_length=100, verbose_name="배달지점링크")
     image = models.ImageField(verbose_name='작성이미지', blank=True, null=True, upload_to='post-image')
     content = models.CharField(max_length=100, verbose_name="내용")
@@ -114,3 +114,4 @@ class RecentSearch(models.Model):
 
             recent_search = cls(user=user, query=query)
             recent_search.save()
+
